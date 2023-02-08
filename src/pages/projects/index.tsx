d3698eb0949dfaa4b23744cc98c9ec578cfa9b5c
Kevin Hermawan <kevin@hyperjump.tech>
2023-02-08 14:29:24 +0700

    feat: adds dashboard page (#5)

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
+function Projects() {
+  return (
+    <DashboardLayout title="Projects">
+      <h1>Projects Page</h1>
+    </DashboardLayout>
+  );
+}
+
+export default Projects;
---
+++ src/pages/templates/index.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
+function Templates() {
+  return (
+    <DashboardLayout title="Templates">
+      <h1>Templates Page</h1>
+    </DashboardLayout>
+  );
+}
+
+export default Templates;
